9304eb66d36a12319dca0a795d4d4e148cab3481
pisichi <win.pisichi@gmail.com>
2023-12-20 21:24:08 +0700

    first commit

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,20 @@
+// src/components/LandingPage.js
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
+      <div className="text-center">
+        <h1 className="mb-4 text-4xl font-bold">Welcome to My Page</h1>
+        <p className="mb-8 text-lg">
+          If I'm not lazy, there should be some thing here soon!
+        </p>
+        <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+          DON'T CLICK
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
