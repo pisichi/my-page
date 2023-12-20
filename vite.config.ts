6dda75c5b42cbf861a25612aad66815e0f808ce1
pisichi <win.pisichi@gmail.com>
2023-12-20 22:02:47 +0700

    vite base

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [react(), eslintPlugin(), viteSvgr(), tsconfigPaths()],
+  base: '/my-page/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
